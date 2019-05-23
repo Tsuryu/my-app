5a465fa1416d4b07814d953ce667207bcd6c28ca
Sergio Velazquez <svelazquez@technisys.com>
2019-05-23 11:27:01 -0300

    fix missing header component fixing styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,8 +5,8 @@ import Logo from '../../assets/graphics/logo.png';
 const Header = (props) => {
     return (
         <header data-test="headerComponent">
-            <div data-test="wrap">
-                <div>
+            <div data-test="wrap" className="wrap">
+                <div className="logo">
                     <img data-test="logoImg" src={Logo} alt="Logo" />
                 </div>
             </div>
